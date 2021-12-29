e566fa7fd39f9facddc40aa5a43836fc980eb95e
eduardogpg <eduardo78d@gmail.com>
2021-12-29 17:53:12 -0600

    Long description

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 from pathlib import Path
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+VERSION = '0.3'
+DESCRIPTION = ''
+
 setup(
     name = 'py_btc_price',
     packages = ['py_btc_price'],
-    version = '0.3',
+    version = VERSION,
     license='MIT',
-    description = 'Get the current Bitcoin Price or Convert the fiat USD equivalente to BTC.',
-    author = 'Eduardo Ismael García Pérez',
+    description = DESCRIPTION,
+    long_description_content_type="text/markdown",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    author = 'Eduardo Ismael García Pérez',
     author_email = 'eduardo78d@gmail.com',
     url = 'https://github.com/eduardogpg/py_btc_price',
     download_url = 'https://github.com/eduardogpg/py_btc_price/archive/refs/tags/0.3.tar.gz',
@@ -22,17 +25,10 @@ setup(
         'requests',
     ],
     classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ],
 )
