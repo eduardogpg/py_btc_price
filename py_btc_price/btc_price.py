24490144aee9a50dd30acf60d3268328eabc703b
eduardogpg <eduardo78d@gmail.com>
2021-12-29 17:00:27 -0600

    New function

--- py_btc_price/btc_price.py
+++ py_btc_price/btc_price.py
@@ -5,3 +5,17 @@ def get_btc_price(currency='USD'):
     
     if response.status_code == 200:
         return  response.json()[currency]['last']
+    
+    return None
+    
+
+def btc_convert(amount):
+    try:
+        current_price = get_btc_price('USD')
+        
+        if current_price:
+            return (1.0 / current_price ) * amount
+        
+    except Exception as err:
+        
+        return None
